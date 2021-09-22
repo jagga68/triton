664298f2188e804756b1440ea796084c9cdbfc36
JAG <jacek.gadomski@gmail.com>
2021-09-22 12:13:20 +0200

    add doctrine CRUD

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -22,11 +22,51 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/home", name="default", name="home")
+     * @Route("/", name="default")
      */
     public function index()
     {
-        //exit('Index here');
+
+        // CRUD CREATE:
+        // $entityManager = $this->getDoctrine()->getManager();
+
+        // $user = new User();
+        // $user->setName('Jacek');
+        // $entityManager->persist($user);
+        // $entityManager->flush();
+        // dump('A new user was saved with the id ' . $user->getId());
+
+        // CRUD READ:
+        // $repository = $this->getDoctrine()->getRepository(User::class);
+        // $user = $repository->find(1);
+        // $user = $repository->findOneBy(['name' => 'Jacek', 'id' => 5]);
+        // $user = $repository->findBy(['name' => 'Jacek'], ['id' => 'DESC']);
+        // $user = $repository->findAll();
+        // dump($user);
+        
+        // CRUD UPDATE
+        // $entityManager = $this->getDoctrine()->getManager();
+        // $id = 2;
+        // $user = $entityManager->getRepository(User::class)->find($id);
+        // if(!$user)
+        // {
+        //     throw $this->createNotFoundException(
+        //         'No user found with id ' . $id
+        //     );
+        // }
+        // $user->setName('Jacol');
+        // $entityManager->flush();
+        // dump($user);
+        
+        // CRUD DELETE
+        $entityManager = $this->getDoctrine()->getManager();
+        $id = 3;
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+                
+                
+
         return $this->render(
             'default/index.html.twig', [
                 'controller_name' => 'DefaultController'
@@ -34,188 +74,189 @@ class DefaultController extends AbstractController
         );
     }
 
-    public function mostPopularPosts($number = 3)
-    {
-        // database call:
-        $posts = ['post 1', 'post 2', 'post 3', 'post 4'];
-        return $this->render(
-            'default/most_popular_posts.html.twig', [
-                'posts' => $posts
-            ]
-        );
-    }
+    // commented out for further reference
+    // public function mostPopularPosts($number = 3)
+    // {
+    //     // database call:
+    //     $posts = ['post 1', 'post 2', 'post 3', 'post 4'];
+    //     return $this->render(
+    //         'default/most_popular_posts.html.twig', [
+    //             'posts' => $posts
+    //         ]
+    //     );
+    // }
    
-    /**
-     * @Route("/", name="default_demo")
-     */
-    public function index_demo(GiftsService $gifts, 
-            Request $request,
-            SessionInterface $session    
-        )
-    {
-        // --------- basics & Doctrine -----------
-        // $users = ['Adam', 'Jacek', 'Piotr', 'Robert'];
+    // /**
+    //  * @Route("/", name="default_demo")
+    //  */
+    // public function index_demo(GiftsService $gifts, 
+    //         Request $request,
+    //         SessionInterface $session    
+    //     )
+    // {
+    //     // --------- basics & Doctrine -----------
+    //     // $users = ['Adam', 'Jacek', 'Piotr', 'Robert'];
 
-        // $user1 = new User();
-        // $user1->setName('Adam');
-        // $user2 = new User();
-        // $user2->setName('Robert');
-        // $user3 = new User();
-        // $user3->setName('Piotr');
-        // $user4 = new User();
-        // $user4->setName('Barbara');
+    //     // $user1 = new User();
+    //     // $user1->setName('Adam');
+    //     // $user2 = new User();
+    //     // $user2->setName('Robert');
+    //     // $user3 = new User();
+    //     // $user3->setName('Piotr');
+    //     // $user4 = new User();
+    //     // $user4->setName('Barbara');
 
-        // $entityManager = $this->getDoctrine()->getManager();
-        // $entityManager->persist($user1);
-        // $entityManager->persist($user2);
-        // $entityManager->persist($user3);
-        // $entityManager->persist($user4);
+    //     // $entityManager = $this->getDoctrine()->getManager();
+    //     // $entityManager->persist($user1);
+    //     // $entityManager->persist($user2);
+    //     // $entityManager->persist($user3);
+    //     // $entityManager->persist($user4);
 
-        // exit($entityManager->flush());
+    //     // exit($entityManager->flush());
 
-        // ------ Cookies --------
-        // $cookie = new Cookie('jag_cookie', 'jag_value', time() + (2 * 365 * 24 * 60 * 60));
-        // $res = new Response();
-        // $res->headers->setCookie($cookie);
-        // $res->send();
+    //     // ------ Cookies --------
+    //     // $cookie = new Cookie('jag_cookie', 'jag_value', time() + (2 * 365 * 24 * 60 * 60));
+    //     // $res = new Response();
+    //     // $res->headers->setCookie($cookie);
+    //     // $res->send();
         
-        // $res = new Response();
-        // $res->headers->clearCookie('jag_cookie');
-        // $res->send();
+    //     // $res = new Response();
+    //     // $res->headers->clearCookie('jag_cookie');
+    //     // $res->send();
         
-        // exit($request->cookies->get('jag_cookie'));
+    //     // exit($request->cookies->get('jag_cookie'));
 
-        // ------ Session --------
-        // $session->set('name', 'session value'); // removes only 
-        // // $session->remove('name');
-        // $session->clear(); // removes all session data
-        // if($session->has('name'))
-        // {
-        //     exit($session->get('name'));
-        // }
+    //     // ------ Session --------
+    //     // $session->set('name', 'session value'); // removes only 
+    //     // // $session->remove('name');
+    //     // $session->clear(); // removes all session data
+    //     // if($session->has('name'))
+    //     // {
+    //     //     exit($session->get('name'));
+    //     // }
         
-        // exit($request->query->get('page', 'default_value'));
-        // exit($request->server->get('HTTP_HOST'));
-        // $request->isXmlHttpRequest(); // is it an Ajax request?
-        // $request->request->get('page');
-        // $request->files->get('foo');
+    //     // exit($request->query->get('page', 'default_value'));
+    //     // exit($request->server->get('HTTP_HOST'));
+    //     // $request->isXmlHttpRequest(); // is it an Ajax request?
+    //     // $request->request->get('page');
+    //     // $request->files->get('foo');
         
-        $this->addFlash('notice', 'Your changes have been saved');
-        $this->addFlash('warning', 'Your changes have warnings');
+    //     $this->addFlash('notice', 'Your changes have been saved');
+    //     $this->addFlash('warning', 'Your changes have warnings');
         
-        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+    //     $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
-        if(!$users)
-        {
-            throw $this->createNotFoundException('The users dont exist');
-        }
+    //     if(!$users)
+    //     {
+    //         throw $this->createNotFoundException('The users dont exist');
+    //     }
         
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-            'users' => $users,
-            'random_gift' => $gifts->gifts,
-        ]);
+    //     return $this->render('default/index.html.twig', [
+    //         'controller_name' => 'DefaultController',
+    //         'users' => $users,
+    //         'random_gift' => $gifts->gifts,
+    //     ]);
 
-    }
+    // }
 
-    /**
-     * @Route("/generate-url/{param?}", name="generate_url")
-     */
-    public function generate_url()
-    {
-        exit($this->generateUrl(
-                'generate_url',
-                array('param' => 10),
-                UrlGeneratorInterface::ABSOLUTE_URL
-            ));
+    // /**
+    //  * @Route("/generate-url/{param?}", name="generate_url")
+    //  */
+    // public function generate_url()
+    // {
+    //     exit($this->generateUrl(
+    //             'generate_url',
+    //             array('param' => 10),
+    //             UrlGeneratorInterface::ABSOLUTE_URL
+    //         ));
 
-    }
+    // }
 
-    /**
-     * @Route("/download")
-     */
-    public function download()
-    {
-        $path = $this->getParameter('download_directory');
+    // /**
+    //  * @Route("/download")
+    //  */
+    // public function download()
+    // {
+    //     $path = $this->getParameter('download_directory');
 
-        return $this->file($path.'file.pdf');
-    }
+    //     return $this->file($path.'file.pdf');
+    // }
 
-    /**
-     * @Route("/redirect-test")
-     */
-    public function redirectTest()
-    {
-        return $this->redirectToRoute('route_to_redirect', array('param' => 10));
-    }
+    // /**
+    //  * @Route("/redirect-test")
+    //  */
+    // public function redirectTest()
+    // {
+    //     return $this->redirectToRoute('route_to_redirect', array('param' => 10));
+    // }
 
-    /**
-     * @Route("/url-to-redirect/{param?}", name="route_to_redirect")
-     */
-    public function methodToRedirect()
-    {
-        exit('Test redirection');
-    }
+    // /**
+    //  * @Route("/url-to-redirect/{param?}", name="route_to_redirect")
+    //  */
+    // public function methodToRedirect()
+    // {
+    //     exit('Test redirection');
+    // }
 
-    /**
-     * @Route("/forwarding-to-controller")
-     */
-    public function forwardingToController()
-    {
-        $response = $this->forward(
-            'App\Controller\DefaultController::methodToForwardTo',
-            array('param' => 5)
-        );
+    // /**
+    //  * @Route("/forwarding-to-controller")
+    //  */
+    // public function forwardingToController()
+    // {
+    //     $response = $this->forward(
+    //         'App\Controller\DefaultController::methodToForwardTo',
+    //         array('param' => 5)
+    //     );
 
-        return $response;
-    }
+    //     return $response;
+    // }
 
-    /**
-     * @Route("/url-to-forwar-to/{param?}", name="rout_to_forward_to")
-     */
-    public function methodToForwardTo($param)
-    {
-        exit('Test controller forwarding - ' . $param);
-    }
+    // /**
+    //  * @Route("/url-to-forwar-to/{param?}", name="rout_to_forward_to")
+    //  */
+    // public function methodToForwardTo($param)
+    // {
+    //     exit('Test controller forwarding - ' . $param);
+    // }
 
-    /**
-     * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
-     */
+    // /**
+    //  * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
+    //  */
 
-     public function index2()
-     {
-         return new Response('Optional parameters in url and requirements for parameters');
-     }
+    //  public function index2()
+    //  {
+    //      return new Response('Optional parameters in url and requirements for parameters');
+    //  }
 
-    /**
-     * @Route("/articles/{_locale}/{year}/{slug}/{category}",
-     * name="articles_list",
-     * defaults={"category" : "computers"},
-     * requirements={
-     *      "_locale" : "en|fr",
-     *      "year" : "\d+",
-     *      "category" : "computers|rtv"
-     * }
-     * 
-     * )
-     */
-    public function index3()
-    {
-        return new Response('An advanced routing example');
-    }
+    // /**
+    //  * @Route("/articles/{_locale}/{year}/{slug}/{category}",
+    //  * name="articles_list",
+    //  * defaults={"category" : "computers"},
+    //  * requirements={
+    //  *      "_locale" : "en|fr",
+    //  *      "year" : "\d+",
+    //  *      "category" : "computers|rtv"
+    //  * }
+    //  * 
+    //  * )
+    //  */
+    // public function index3()
+    // {
+    //     return new Response('An advanced routing example');
+    // }
 
-    /**
-     * @Route({
-     *      "nl" : "/over-ons",
-     *      "en" : "/about-us",
-     *      "pl" : "/o-nas"
-     *  }, name="about_us"
-     * )
-     */
-    public function index4()
-    {
-        return new Response('Translated routes');
-    }
+    // /**
+    //  * @Route({
+    //  *      "nl" : "/over-ons",
+    //  *      "en" : "/about-us",
+    //  *      "pl" : "/o-nas"
+    //  *  }, name="about_us"
+    //  * )
+    //  */
+    // public function index4()
+    // {
+    //     return new Response('Translated routes');
+    // }
 
 
 }
---
+++ my-project/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load (ObjectManager $manager)
+    {
+        for($i=1; $i<=5; $i++)
+        {
+            $user = new User();
+            $user->setName('name' . $i);
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+}
+
+?>
